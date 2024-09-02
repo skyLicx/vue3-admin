9108245b3dccc963392ba26d6ac38d41ac985ed5
licx <skylicx@163.com>
2024-09-02 15:55:25 +0800

    element plus中英文切换

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <template>
-  <router-view />
+  <el-config-provider :locale="locale">
+    <router-view />
+  </el-config-provider>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue'
+import zhCN from 'element-plus/dist/locale/zh-cn.mjs'
+import enUS from 'element-plus/dist/locale/en.mjs'
+import useLocale from '@/hooks/useLocale'
+
+const { currentLocale } = useLocale()
+const locale = computed(() => {
+  switch (currentLocale.value) {
+    case 'zh-CN':
+      return zhCN
+    case 'en-US':
+      return enUS
+    default:
+      return zhCN
+  }
+})
+</script>
 
 <style lang="scss"></style>
